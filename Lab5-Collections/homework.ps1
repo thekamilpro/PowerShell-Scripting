ea93d52f058b86f99490732c89a0582dbdb78fa2
Kamil Pro <kprocyszyn@gmail.com>
2025-07-06 15:26:48 +0100

    add homework

---
+++ Lab5-Collections/homework.ps1
@@ -0,0 +1,9 @@
+<#
+Using data structures you learnt in this module, create a function that builds a file tree from a given path
+The file tree should keep a structure of folders and files
+Start with Get-ChildItem -Path parameter, do not use "Recurse" parameter!
+Tips:
+    - You can use GetType() to distinguish between folders and files
+    - You might need to use a few data structures e.g. hashtable and an array to achieve this goal
+    - You might find writing your own recursion particularly useful in this exercise
+#> 
